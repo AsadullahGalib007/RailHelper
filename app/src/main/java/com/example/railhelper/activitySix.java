097da9104ecf5563d76b831c98e466f32a746048
AsadullahGalib007 <abrgalib@gmail.com>
2021-02-20 22:37:52 +0600

    So far so good

---
+++ app/src/main/java/com/example/railhelper/activitySix.java
@@ -0,0 +1,38 @@
+package com.example.railhelper;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class activitySix extends AppCompatActivity {
+
+    private WebView webView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_six);
+
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://esheba.cnsbd.com")));
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == android.R.id.home){
+            this.finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
